249dadd6a8b6b0183d5f1ff8130ec5001241fc8d
Shahriar Mahmud Tanim <157669438+tanimweb1@users.noreply.github.com>
2026-01-18 23:23:22 +0600

    Add files via upload

---
+++ Day-32.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main(){
+
+
+// #F8EFE0
+// #FBF4E7
+// for loop 
+
+int n;
+scanf("%d",&n);
+
+for(int i = 1;i<=10;i++){
+    printf("%d * %d = %d\n",n,i,n*i);
+}
+
+
+
+
+
+return 0;
+
+}
